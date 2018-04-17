762e9490682846f1cb3e5bdaefe1a2931a7cd7df
nekogato <05kcwong@gmail.com>
2018-04-17 16:00:44 +0800

    Add files via upload

---
+++ theme_eats365/__manifest__.py
@@ -0,0 +1,13 @@
+{
+  'name':'Eats365 theme',
+  'description': 'A Eats365 Theme.',
+  'version':'1.0',
+  'author':'Nekogato',
+
+  'data': [
+	  'views/layout.xml',
+	  'views/pages.xml'
+	],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
